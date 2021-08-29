836112a43292f7bd0d6ababf98f65f6962391750
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-08-29 21:33:34 +1000

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shoppage";
+import SignInAndSignUpPage from "./pages/signin-signup/signin-signup";
+
 import Header from "./components/header/header";
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
