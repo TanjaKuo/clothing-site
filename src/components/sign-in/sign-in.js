a7a00f3b0a419b824f2e4058c123b09153350645
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-08-31 21:05:16 +1000

    update

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.scss";
 
@@ -16,9 +16,15 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ email: "", password: "" });
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   handleChange = (e) => {
