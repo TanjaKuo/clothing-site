c110c3c79d7fd5f0288c481cecbabf7f6791a85e
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-09-02 22:58:05 +1000

    clear collection-item code

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import CustomButton from "../custom-button/custom-button";
+
 import "./collection-item.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -10,31 +12,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
 
 export default CollectionItem;
-
-/* 
-import React from "react";
-
-import "./collection-item.scss";
-
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
-); 
-
-export default CollectionItem;
-*/
