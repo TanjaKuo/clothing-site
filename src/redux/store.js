0c192ecf5955f10c6cfd85c1c1a7cdb8da77caf4
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-09-04 15:24:30 +1000

    added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
-
 import logger from "redux-logger"; // for debugging
+import { persistStore } from "redux-persist"; // allow browser to cache store
 
 import rootReducer from "./root-reducer";
 
@@ -8,5 +8,6 @@ const middlewares = [logger]; // can be multiple
 
 //const store = createStore(rootReducer, appleMiddleware(logger)); if middleware only take one, can be write like this
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
