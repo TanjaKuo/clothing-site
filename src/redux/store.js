6134c31df81d82bc920487e671ed75b725b029d0
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-09-06 10:53:11 +1000

    remove redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import { persistStore } from "redux-persist"; // allow browser to cache store
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger]; // can be multiple
+const middlewares = []; // can be multiple
+
+// there are three types "development" || "production" || "test" in NODE_ENV
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 //const store = createStore(rootReducer, appleMiddleware(logger)); if middleware only take one, can be write like this
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
