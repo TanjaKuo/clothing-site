6497463bd54df2637c4c1664fded3900042dd4bb
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-09-20 19:01:46 +1000

    using redux-saga to replace redux-thunk, in order to async collection

---
+++ src/redux/shop/shop.sages.js
@@ -0,0 +1,45 @@
+// move collection from action to saga
+// takeEvery -> listen for evry action of specific type that we pass to
+
+import { takeEvery, put, call } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "../shop/shop.action.js";
+
+import { ShopActionTypes } from "./shop.types";
+
+export function* fetchCollectionAsync() {
+  try {
+    const collectionRef = firestore.collection("collection");
+    const snapshot = yield collectionRef.get();
+    //const collectionsMap = convertCollectionsSnapshotToMap(snapshot), we can do this, but we want to yield and call this, in case it takes longer than we expect
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+  /* collectionRef
+    .get()
+    .then((snapshot) => {
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(collectionMap));
+    })
+    .catch((error) => dispatch(fetchCollectionsFailure(error.message))); */
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionAsync
+  );
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger"; // for debugging
 import { persistStore } from "redux-persist"; // allow browser to cache store
-import thunk from "redux-thunk";
+//import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
+import { fetchCollectionsStart } from "./shop/shop.sages";
 import rootReducer from "./root-reducer";
 
-const middlewares = [thunk]; // can be multiple
+const sageMiddleware = createSagaMiddleware();
+
+const middlewares = [sageMiddleware]; // can be multiple
 
 // there are three types "development" || "production" || "test" in NODE_ENV
 if (process.env.NODE_ENV === "development") {
@@ -14,6 +18,9 @@ if (process.env.NODE_ENV === "development") {
 
 //const store = createStore(rootReducer, appleMiddleware(logger)); if middleware only take one, can be write like this
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sageMiddleware.run(fetchCollectionsStart);
+
 const persistor = persistStore(store);
 
 export { store, persistor };
